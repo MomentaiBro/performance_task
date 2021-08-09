197423a7ac6cf82182bf1c7b1ff07f4d0049e9bf
Alexander Sherwin <alx.sherwin@gmail.com>
2021-08-08 20:21:09 -0600

    added colors and played around with some hove styling

--- src/ui/components/UserTable.tsx
+++ src/ui/components/UserTable.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState }  from 'react';
 import Filter from './Filter';
+import '../stylesheets/main.css';
 
 type UserType = {
     id: number;
@@ -42,38 +43,43 @@ const UserTable: React.FC = () => {
     return (
         <div className="admin-user-table" style={{ marginTop: '2rem'}}>
             <Filter />
-            <div style={{width: '90vw', marginTop: '2rem'}}>
+            <div style={{width: '100vw', marginTop: '2rem'}}>
                 <h2 style={{textAlign: 'center', textDecoration: 'underline'}}> Users </h2>
                 <ul style={{listStyle: 'none', paddingLeft: 0, height: '30rem', overflow: "auto"}}>
-                    <li style={{position: 'sticky', background: "gray", top: 0, fontWeight: 700, borderBottom: '2px solid black', marginBottom: '1rem', padding: '1rem'}}>
-                        <div style={{ border: '1px solid red', display: 'flex', justifyContent: 'space-evenly', textAlign: 'center'}}>
+                    <li className="admin-user-table-legend" style={{position: 'sticky', top: 0, fontWeight: 700, borderBottom: '1px solid black', marginBottom: '1rem', padding: '1rem'}}>
+                        <div style={{display: 'flex', justifyContent: 'space-evenly', textAlign: 'center'}}>
                             <div style={{width: '2%'}}>ID</div>
-                            <div style={{width: '15%'}}>Last Name</div>
-                            <div style={{width: '20%'}}>First Name</div>
-                            <div style={{width: '5%'}}>M.I.</div>
+                            <div style={{width: '10%'}}>Last Name</div>
+                            <div style={{width: '10%'}}>First Name</div>
+                            <div style={{width: '2%'}}>M.I.</div>
                             <div style={{width: '20%'}}>District</div>
-                            <div style={{width: '10%'}}>Verified</div>
-                            <div style={{width: '20%'}}>Created</div>
-                            <div style={{width: '20%'}}>Email</div>
+                            <div style={{width: '5%'}}>Verified</div>
+                            <div style={{width: '15%'}}>Created</div>
+                            <div style={{width: '15%'}}>Email</div>
+                            <div style={{width: '5%'}}> Edit</div>
                         </div>
                     </li>
 
                     {users && users.map(user => { 
                         return (
-                            <li key={user.id} style={{marginBottom: '2rem', background: '#fff', border: '1px solid black', padding: '1rem'}}>
-                                <div style={{display: 'flex', justifyContent: 'space-evenly', textAlign: 'center', marginBottom: '0.5rem'}}>
+                            <li 
+                                key={user.id}
+                                className="user_block"
+                                style={{marginBottom: '.5rem', padding: '.5rem'}}>
+                                
+                                <div  style={{display: 'flex', justifyContent: 'space-evenly', textAlign: 'center', alignItems: 'center', marginBottom: '0.5rem'}}>
                                     <div style={{width: '2%'}}>{user.id}</div>
-                                    <div style={{width: '15%'}}>{user.last_name}</div>
-                                    <div style={{width: '20%'}}>{user.first_name}</div>
-                                    <div style={{width: '5%'}}>{user.middle_initial}</div>
+                                    <div style={{width: '10%'}}>{user.last_name}</div>
+                                    <div style={{width: '10%'}}>{user.first_name}</div>
+                                    <div style={{width: '2%'}}>{user.middle_initial}</div>
                                     <div style={{width: '20%'}}>{user.district}</div>
-                                    <div style={{width: '10%'}}>{user.verified}</div>
-                                    <div style={{width: '20%'}}>{user.created_at}</div>
-                                    <div style={{width: '20%'}}>{user.email}</div>
-                                </div>
-                                <div style={{marginLeft: 'auto', width: '10rem', display: 'flex', justifyContent: 'space-between', paddingRight: '2rem'}}>
-                                    <button type="button">Edit</button>
-                                    <button type="button">Delete</button>
+                                    <div style={{width: '5%'}}>{user.verified}</div>
+                                    <div style={{width: '15%'}}>{user.created_at}</div>
+                                    <div style={{width: '15%'}}>{user.email}</div>
+                                    <div style={{width: '5%', display: 'flex', justifyContent: 'space-between', paddingRight: '2rem'}}>
+                                        <button type="button">Edit</button>
+                                        <button type="button">Delete</button>
+                                    </div>
                                 </div>
                             </li>
                         );
